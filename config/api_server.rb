6764c9e2618a09773facfc09300072e8316eeb5a
Toni Förster <toni.foerster@gmail.com>
2018-05-10 19:46:58 +0200

    forgot the tdl :-(

--- config/api_server.rb
+++ config/api_server.rb
@@ -1,5 +1,5 @@
 class WadokuSearchClient < Sinatra::Base
-  set :api_host, "https://wadoku:10020"
+  set :api_host, "https://wadoku.eu:10020"
   set :root, ROOT_DIR
   set :views, settings.root + "/app/views"
   set :static, true
